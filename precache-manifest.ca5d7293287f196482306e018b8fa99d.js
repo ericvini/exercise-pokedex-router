e48d50eb605273235aaa48811f7f69d3accc444c
ericvini <ericcobain3@gmail.com>
2020-11-22 17:36:46 -0300

    Updates

---
+++ precache-manifest.ca5d7293287f196482306e018b8fa99d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a08f1d169a6dd8235f70665de7fcccf",
+    "url": "/my-pokedex/index.html"
+  },
+  {
+    "revision": "7b468936d800eb42831e",
+    "url": "/my-pokedex/static/css/main.caa71708.chunk.css"
+  },
+  {
+    "revision": "909cdb8c228c47511fc6",
+    "url": "/my-pokedex/static/js/2.601a0fcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-pokedex/static/js/2.601a0fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b468936d800eb42831e",
+    "url": "/my-pokedex/static/js/main.603f9375.chunk.js"
+  },
+  {
+    "revision": "3ce3f06a6982683bd48f",
+    "url": "/my-pokedex/static/js/runtime-main.5beaabc1.js"
+  }
+]);
